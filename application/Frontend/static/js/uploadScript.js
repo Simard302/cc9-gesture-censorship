08182b56a0ba3ca6947a001c58d702c341e23283
Eric Huang <eric.huang3143@gmail.com>
2023-07-08 15:45:03 -0400

    Broke something., File uploard broken

--- application/Frontend/static/js/uploadScript.js
+++ application/Frontend/static/js/uploadScript.js
@@ -20,12 +20,39 @@ fileInput.onchange = ({target})=>{
 }
 
 function uploadFile(name){
+
+  // let fileInput = document.getElementById("file-input");
+  // let file = fileInput.files[0]; // Get the selected file
+
+  //   if (file.size >= 1073741824) {
+  //     progressArea.innerHTML = "";
+  //     uploadedArea.classList.add("onprogress");
+      
+  //     progressArea.innerHTML = progressHTML;
+  //     let uploadedHTML = `<li class="row">
+  //                           <div class="content upload">
+  //                             <i class="fas fa-file-alt"></i>
+  //                             <div class="details">
+  //                               <span class="name">${files[i].name}: File size exceeds 1GB. File not uploaded.</span>
+  //                               <span class="size">${fileSize}</span>
+  //                             </div>
+  //                           </div>
+  //                           <i class="fa-solid fa-x"></i>
+  //                         </li>`
+  //     uploadedArea.classList.remove("onprogress");
+  //     uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML);
+  // }
+  
+
+
   let xhr = new XMLHttpRequest();
-  xhr.open("POST", "/api/upload");
+  xhr.open("POST", "api/upload");
   xhr.upload.addEventListener("progress", ({loaded, total}) =>{
+
     let fileLoaded = Math.floor((loaded / total) * 100);
     let fileTotal = Math.floor(total / 1000);
     let fileSize;
+
     (fileTotal < 1024) ? fileSize = fileTotal + " KB" : fileSize = (loaded / (1024*1024)).toFixed(2) + " MB";
     let progressHTML = `<li class="row">
                           <i class="fas fa-file-alt"></i>
@@ -58,3 +85,4 @@ function uploadFile(name){
     }
   });
 }
+
