a75fb02b5db6bd2bcb14ef2b05851b640cc0b8fc
Eric Huang <eric.huang3143@gmail.com>
2023-07-09 17:13:49 -0400

    Made the stream constantly call post, change streaming script to reduce time between post requests

--- application/Frontend/static/js/StreamScript.js
+++ application/Frontend/static/js/StreamScript.js
@@ -1,10 +1,6 @@
 const form = document.querySelector("form"),
 video = document.querySelector("video");
 
-// form.addEventListener("click", () =>{
-//   startup();
-// });
-
 function startup(){
   console.log("Startup");
   navigator.mediaDevices.getUserMedia({
@@ -17,7 +13,64 @@ function startup(){
     }
   }).then(stream => {
     video.srcObject = stream;
-  }).catch(console.error)
+    console.log("Stream Set");
+
+    // Start streaming to Django REST API
+    var apiEndpoint = 'api/upload';
+    var mediaRecorder = new MediaRecorder(stream);
+    var frameCount = 0;
+    var chunks = [];
+
+    console.log("Variables Set");
+
+    mediaRecorder.ondataavailable = function (e) {
+      console.log(`Frame Count: ${frameCount}`);
+
+      chunks.push(e.data);
+      frameCount++;
+
+      if (frameCount === 15) {
+        mediaRecorder.stop();
+      }
+    };
+
+    mediaRecorder.onstop = function () {
+      var blob = new Blob(chunks, { type: 'video/webm' });
+      var formData = new FormData();
+      formData.append('video', blob, 'stream.webm');
+
+      // Send the video blob to Django REST API using fetch
+      fetch(apiEndpoint, {
+        method: 'POST',
+        body: formData
+      })
+        .then(function (response) {
+          console.log('Video uploaded successfully!');
+          frameCount = 0;
+          chunks = [];
+          setTimeout(startRecording,1);
+        })
+        .catch(function (error) {
+          console.error('Error uploading video:', error);
+          frameCount = 0;
+          chunks = [];
+          setTimeout(startRecording,1); // Delay for 1 second before starting the next recording
+        });
+    };
+
+    function startRecording() {
+      if (mediaRecorder.state !== 'recording') {
+        mediaRecorder.start(10);
+      }
+    }
+
+    // Start recording the video stream
+    startRecording();
+  })
+
+  .catch(function (error) {
+    console.error('Error accessing camera:', error);
+  });
 }
 
 window.addEventListener('load',startup, false);
